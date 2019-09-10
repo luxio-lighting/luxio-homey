6d8598b888f09ce6574eb0ded6bc3c639de13c75
Emile Nijssen <emilenijssen@gmail.com>
2019-09-10 23:03:44 +0200

    mdns discovery

--- drivers/luxio/device.js
+++ drivers/luxio/device.js
@@ -2,74 +2,91 @@
 
 const Homey = require('homey');
 const tinycolor = require('tinycolor2');
+const { Device: LuxioDevice } = require('luxio');
 
 const SYNC_INTERVAL = 7500;
 
 module.exports = class LuxioDevice extends Homey.Device {
-  
+
   onInit() {
     this.sync = this.sync.bind(this);
-    
+
     this.id = this.getData().id;
     this._driver = this.getDriver();
     this.setUnavailable( new Error('Offline') );
-    
+
     this._driver.ready(() => {
       this._driver.getDevice(this.id).then(device => {
         this._device = device;
-        
+
         this._hue = this.getCapabilityValue('light_hue');
         if( this._hue === null ) this._hue = 0;
-        
+
         this._saturation = this.getCapabilityValue('light_saturation');
         if( this._saturation === null ) this._saturation = 1;
-        
+
         this._temperature = this.getCapabilityValue('light_saturation');
         if( this._temperature === null ) this._temperature = 0.5;
-        
+
         this._mode = this.getCapabilityValue('light_mode');
         if( this._mode === null ) this._mode = 'color';
-        
+
         this.syncInterval = setInterval(this.sync, SYNC_INTERVAL);
         this.sync();
-        
+
       }).catch(err => {
         this.error(err);
         this.setUnavailable(err);
-      });    
+      });
     });
-      
+
     this.registerCapabilityListener('onoff', this.onCapabilityOnoff.bind(this));
     this.registerCapabilityListener('dim', this.onCapabilityDim.bind(this));
     this.registerMultipleCapabilityListener(['light_hue', 'light_saturation'], this.onCapabilityLightHueSat.bind(this));
     this.registerCapabilityListener('light_temperature', this.onCapabilityLightTemperature.bind(this));
     this.registerCapabilityListener('light_mode', this.onCapabilityLightMode.bind(this));
-    
+
   }
-  
+
+  onDiscoveryResult(discoveryResult) {
+    return discoveryResult.id === this.getData().id;
+  }
+
+  onDiscoveryAvailable(discoveryResult) {
+    this._device = new LuxioDevice(discoveryResult.id, {
+      address: discoveryResult.address,
+      name: discoveryResult.txt.name,
+      version: discoveryResult.txt.version,
+    });
+  }
+
+  onDiscoveryAddressChanged(discoveryResult) {
+    this._device.address = discoveryResult.address;
+  }
+
   onRenamed( name ) {
     this._device.name = name;
     this._device.sync().catch(this.error);
   }
-  
+
   onDeleted() {
     if( this.syncInterval )
       clearInterval(this.syncInterval);
   }
-  
+
   sync() {
     this._device.sync().then(() => {
       this.setCapabilityValue('onoff', this._device.on);
       this.setCapabilityValue('dim', this._device.brightness);
-      
-      if( this._device.mode === 'gradient' ) {    
+
+      if( this._device.mode === 'gradient' ) {
         const color = this._device.gradient[0];
         const hsv = tinycolor(`${color}`).toHsv();
-        
-        if( this._mode === 'color' ) {          
+
+        if( this._mode === 'color' ) {
           this._hue = hsv.h/360;
           this._saturation = hsv.s;
-          
+
           this.setCapabilityValue('light_hue', this._hue);
           this.setCapabilityValue('light_saturation', this._saturation);
         } else if( this._mode === 'temperature' ) {
@@ -79,65 +96,65 @@ module.exports = class LuxioDevice extends Homey.Device {
       } else if( this._device.mode === 'effect' ) {
         // ...
       }
-      
+
       this.setAvailable();
     }).catch( err => {
       this.error(err);
       this.setUnavailable(err);
-    });    
+    });
   }
-  
+
   async onCapabilityOnoff( value ) {
     this._device.on = value;
     return this.sync();
   }
-  
+
   async onCapabilityDim( value ) {
     this._device.on = ( value > 0 );
     this._device.brightness = value;
     return this.sync();
   }
-  
+
   async onCapabilityLightHueSat( values ) {
     if( typeof values.light_hue === 'number' )
       this._hue = values.light_hue;
-      
+
     if( typeof values.light_saturation === 'number' )
       this._saturation = values.light_saturation;
-      
+
     this._mode = 'color';
     return this._setColor();
   }
-  
+
   async onCapabilityLightTemperature( value ) {
     this._temperature = value;
     this._mode = 'temperature';
-    return this._setColor();  
-    
+    return this._setColor();
+
   }
-  
+
   async onCapabilityLightMode( value ) {
     this._mode = value;
-    return this._setColor();    
+    return this._setColor();
   }
-  
-  async _setColor() {    
+
+  async _setColor() {
     if( this._mode === 'color' ) {
       this._device.color = tinycolor({
         h: this._hue * 360,
         s: this._saturation,
         l: 0.5
-      }).toHexString();    
+      }).toHexString();
     } else if( this._mode === 'temperature' ) {
       this._device.colorTemperature = this._temperature;
     }
-    
+
     return this.sync();
   }
-  
+
   async setEffect( effect ) {
     this._device.effect = effect;
     return this.sync();
   }
-  
+
 }
--- drivers/luxio/driver.js
+++ drivers/luxio/driver.js
@@ -1,58 +1,30 @@
 'use strict';
 
 const Homey = require('homey');
-const Discovery = require('luxio').Discovery;
-
-const POLL_INTERVAL = 1000 * 30;
 
 module.exports = class LuxioDriver extends Homey.Driver {
-  
-  onInit() {    
-    this._devices = {};    
-    this._discovery = new Discovery();
-    
-    this.pollDevices = this.pollDevices.bind(this);
-    this.pollDevices();
-    setInterval(this.pollDevices, POLL_INTERVAL);
-      
+
+  onInit() {
     new Homey.FlowCardAction('set_effect')
       .register()
       .registerRunListener(async args => {
         return args.device.setEffect(args.effect);
       })
-    
   }
-  
-  async getDevice(id) {
-    const device = this._devices[id];
-    if(device) return device;
-    
-    return new Promise(resolve => {
-      this.once(`device_${id}`, resolve);
-    });
-  }
-  
-  pollDevices() {    
-    this._discovery.getDevices().then(devices => {
-      Object.values(devices).forEach(device => {
-        if( this._devices[device.id] ) return;
-        
-        this._devices[device.id] = device;
-        this.emit(`device_${device.id}`, device);
-      });
-    }).catch( this.error );
-  }
-  
+
   onPairListDevices( data, callback ) {
-    const devices = Object.values(this._devices).map(device => {      
+    const discoveryStrategy = this.getDiscoveryStrategy();
+    const discoveryResults = discoveryStrategy.getDiscoveryResults();
+
+    const devices = Object.values(discoveryResults).map(discoveryResult => {
       return {
         data: {
-          id: device.id,
+          id: discoveryResult.id,
         },
-        name: device.name
+        name: discoveryResult.txt.name,
       };
     });
     callback( null, devices );
   }
-  
+
 }
